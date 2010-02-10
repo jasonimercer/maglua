2dd08f6075384375dcd5a78e9a5425c2ddc25e01
Jason Mercer <jason.mercer@gmail.com>
2010-02-10 13:54:07 +0000

    Formatting for premake info
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@3 01fa2656-f03c-45d1-8fba-b86aa1ed0177

--- src/premake.sh
+++ src/premake.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-REVISION=`svn info | grep Revision`
+REVISION=`svn info | grep Revision | sed s/\:/\:\ \ \ \ /`
 AUTHOR=`svn info | grep Author:`
-DATE=`svn info | grep Date:`
-
-# cat > info.h
+DATE=`svn info | grep Date: | sed s/Date\:/Date\:\ \ /`
+COMPILER=`$1 --version | head -n 1`;
 
 cat > info.h <<EOF
 const char* __info ="Maglua $REVISION\\n\\
 $AUTHOR\\n\\
-$DATE\\n";
+$DATE\\n\\
+Compiler:            $COMPILER\\n";
 EOF
-
-
-# echo $SRC > info.h
-
