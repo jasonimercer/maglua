8bef58cdf7d9e688ab87da9bd8c5c2273051956c
Jason Mercer <jason.mercer@gmail.com>
2010-02-22 18:41:57 +0000

    added more info to info.sh
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@23 01fa2656-f03c-45d1-8fba-b86aa1ed0177

--- src/premake.sh
+++ src/premake.sh
@@ -4,6 +4,7 @@ REVISION=`svn info | grep Revision | sed s/\:/\:\ \ \ \ /`
 AUTHOR=`svn info | grep Author:`
 DATE=`svn info | grep Date: | sed s/Date\:/Date\:\ \ /`
 COMPILER=`$1 --version | head -n 1`;
+COMPILELINE="$1"
 
 cat > info.h <<EOF
 const char* __info ="\\
@@ -11,5 +12,6 @@ Maglua $REVISION\\n\\
 $AUTHOR\\n\\
 $DATE\\n\\
 Compiler:            $COMPILER\\n\\
+Compile Line:        $COMPILELINE\\n\\
 ";
 EOF
