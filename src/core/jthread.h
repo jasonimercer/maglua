34f17ef269123d3de845a8bd6c94f2dbaf4eee1c
Jason Mercer <jason.mercer@gmail.com>
2011-08-02 13:16:44 +0000

    these were missing
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@126 01fa2656-f03c-45d1-8fba-b86aa1ed0177

---
+++ src/core/jthread.h
@@ -0,0 +1,31 @@
+#ifndef JASONTHREAD
+#define JASONTHREAD
+
+#include <pthread.h>
+
+class JThread
+{
+public:
+	JThread(void *(*start_routine)(void*), void* arg);
+	~JThread();
+	int start();
+
+	enum jstate
+	{
+		unstarted,
+		running,
+		complete
+	};
+
+	jstate state;
+	
+	void* call_from_wrapper();
+	int join();
+private:
+	void* (*start_routine)(void*);
+	void* arg;
+	
+	pthread_t thread;
+};
+
+#endif
