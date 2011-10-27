bdaebcc6fc74f1412f2baeb043be1dcdb22f307a
Jason Mercer <jason.mercer@gmail.com>
2011-10-27 18:05:20 +0000

    still moving things
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@151 01fa2656-f03c-45d1-8fba-b86aa1ed0177

--- src/core_purecuda/llgquat.hpp
+++
@@ -1,6 +0,0 @@
-void cuda_llg_quat_apply(const int nx, const int ny, const int nz,
-	double* dsx, double* dsy, double* dsz, double* dms, //dest
-	double* ssx, double* ssy, double* ssz, double* sms, // src
-	double* hx, double* hy, double* hz,
-	double* ws1, double* ws2, double* ws3, double* ws4,
-	const double alpha, const double dt, const double gamma);
