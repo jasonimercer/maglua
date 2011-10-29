5bed02530c27c17f6572669edda8febd87a45886
Jason Mercer <jason.mercer@gmail.com>
2011-10-29 00:03:06 +0000

    working on cuda_info
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@159 01fa2656-f03c-45d1-8fba-b86aa1ed0177

--- src/modules/cuda/core_cuda/maglua.cpp
+++ src/modules/cuda/core_cuda/maglua.cpp
@@ -11,6 +11,8 @@
 ******************************************************************************/
 
 #include "luacommon.h"
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 extern "C"
 {
@@ -28,7 +30,8 @@ CORECUDA_API void lib_main(lua_State* L, int argc, char** argv);
 #include "info.h"
 #include "spinsystem.h"
 #include "spinoperation.h"
-	
+#include <string.h>
+
 CORECUDA_API int lib_register(lua_State* L)
 {
 	registerSpinSystem(L);
@@ -46,6 +49,127 @@ CORECUDA_API const char* lib_name(lua_State* L)
 	return "Core-Cuda";
 }
 
+
+#if CUDART_VERSION >= 2000
+static int ConvertSMVer2Cores(int major, int minor)
+{
+	// Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+	typedef struct {
+			int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+			int Cores;
+	} sSMtoCores;
+
+	sSMtoCores nGpuArchCoresPerSM[] = 
+	{ { 0x10,  8 },
+		{ 0x11,  8 },
+		{ 0x12,  8 },
+		{ 0x13,  8 },
+		{ 0x20, 32 },
+		{ 0x21, 48 },
+		{   -1, -1 }
+	};
+
+	int index = 0;
+	while (nGpuArchCoresPerSM[index].SM != -1)
+	{
+		if(nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) )
+		{
+			return nGpuArchCoresPerSM[index].Cores;
+		}
+                index++;
+	}
+	printf("MapSMtoCores undefined SMversion %d.%d!\n", major, minor);
+	return -1;
+}
+#endif
+
+// This function wraps the CUDA Driver API into a template function
+template <class T>
+inline void getCudaAttribute(T *attribute, CUdevice_attribute device_attribute, int device)
+{
+    CUresult error =    cuDeviceGetAttribute( attribute, device_attribute, device );
+
+    if( CUDA_SUCCESS != error) {
+        fprintf(stderr, "cuSafeCallNoSync() Driver API error = %04d from file <%s>, line %i.\n",
+                error, __FILE__, __LINE__);
+        exit(-1);
+    }
+}
+
+
 CORECUDA_API void lib_main(lua_State* L, int argc, char** argv)
 {
+	cudaSetDevice(1);
+
+	
+	int driverVersion;
+	int runtimeVersion;
+	int deviceCount;
+	int device;
+	
+	cudaGetDeviceCount(&deviceCount);
+	cudaGetDevice(&device);
+	cudaDriverGetVersion(&driverVersion);
+	cudaRuntimeGetVersion(&runtimeVersion);
+
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, device);
+
+	for(int i=0; i<argc; i++)
+	{
+		if(strcasecmp(argv[i], "-cuda_info") == 0)
+		{
+
+
+			
+			printf("CUDA Info\n");
+			printf("-------------------------------\n");
+			printf("Device                 %d/%d\n", device, deviceCount);
+        	printf("Driver Version         %d.%d\n", driverVersion/1000, driverVersion%100);
+			printf("Runtime Version        %d.%d\n", runtimeVersion/1000, runtimeVersion%100);
+			printf("Global Memory     %6.0f MB\n",  (float)deviceProp.totalGlobalMem/1048576.0f);
+			#if CUDART_VERSION >= 2000
+			printf("Multiprocessors      %3d\n",  deviceProp.multiProcessorCount);
+			printf("CUDA Cores/MP        %3d\n",  ConvertSMVer2Cores(deviceProp.major, deviceProp.minor));
+			printf("CUDA Cores           %3d\n",  ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) * deviceProp.multiProcessorCount);
+			#endif
+			#if CUDART_VERSION >= 4000
+			int memoryClock;
+			getCudaAttribute<int>( &memoryClock, CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE, device );
+			printf("Memory Clock rate:  %.2f Mhz\n", memoryClock * 1e-3f);
+			int memBusWidth;
+			getCudaAttribute<int>( &memBusWidth, CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH, device );
+			printf("Memory Bus Width:  %5d bits\n", memBusWidth);
+			int L2CacheSize;
+			getCudaAttribute<int>( &L2CacheSize, CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE, device );
+        	if(L2CacheSize)
+			{
+				printf("L2 Cache Size:%10d bytes\n", L2CacheSize);
+			}
+
+			#endif
+			
+			printf("Constant memory:%8d bytes\n", (int)deviceProp.totalConstMem); 
+			printf("Shared mem/block:%7d bytes\n", (int)deviceProp.sharedMemPerBlock);
+			printf("Registers/block: %7d\n", (int)deviceProp.regsPerBlock);
+			printf("Warp size:       %7d\n", (int)deviceProp.warpSize);
+			printf("Max threads/block:%6d\n", (int)deviceProp.maxThreadsPerBlock);
+// 			printf("Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+//                deviceProp.maxThreadsDim[0],
+//                deviceProp.maxThreadsDim[1],
+//                deviceProp.maxThreadsDim[2]);
+// 			printf("Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+//                deviceProp.maxGridSize[0],
+//                deviceProp.maxGridSize[1],
+//                deviceProp.maxGridSize[2]);
+// 			printf("  Maximum memory pitch:                          %u bytes\n", deviceProp.memPitch);
+// 			printf("  Texture alignment:                             %u bytes\n", deviceProp.textureAlignment);
+
+			
+			printf("-------------------------------\n");
+			
+			
+			return;
+		}
+	}
 }
