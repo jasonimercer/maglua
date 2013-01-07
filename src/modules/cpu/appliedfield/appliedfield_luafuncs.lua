d8cdb6c737b7c1feaa3f945fdca7af7ea89ea5c9
Jason Mercer <jason.mercer@gmail.com>
2013-01-07 17:14:16 +0000

    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@281 01fa2656-f03c-45d1-8fba-b86aa1ed0177

---
+++ src/modules/cpu/appliedfield/appliedfield_luafuncs.lua
@@ -0,0 +1,85 @@
+-- AppliedField 
+
+local MODNAME = "AppliedField"
+local MODTAB = _G[MODNAME]
+local t = maglua_getmetatable(MODNAME) -- this is a special function available only at registration time
+
+local function set(a, x,y,z)
+	if type(x) == "table" then
+		z = x[3] or 0
+		y = x[2] or 0
+		x = x[1] or 0
+	else
+		x = x or 0
+		y = y or 0
+		z = z or 0
+	end
+	
+	a:setX(x)
+	a:setY(y)
+	a:setZ(z)
+end
+
+	
+local function add(a, x,y,z)
+	if type(x) == "table" then
+		z = x[3] or 0
+		y = x[2] or 0
+		x = x[1] or 0
+	end
+	
+	a:setX(a:x() + x)
+	a:setY(a:y() + y)
+	a:setZ(a:z() + z)
+end
+
+	
+local function get(a)
+	return a:x(), a:y(), a:z()
+end
+ 
+
+local function apply(a, ss)
+	local name = "applied"
+	local scale = a:scale()
+	ss:fieldArrayX(name):setAll(a:x() * scale)
+	ss:fieldArrayY(name):setAll(a:y() * scale)
+	ss:fieldArrayZ(name):setAll(a:z() * scale)
+	ss:setSlotUsed(name)
+end
+
+t.set = set
+t.get = get
+t.add = add
+t.apply = apply
+
+local help = MODTAB.help
+
+MODTAB.help = function(x)
+	if x == set then
+		return 
+			"Set the direction and strength of the Applied Field",
+			"1 *3Vector*: The *3Vector* defines the strength and direction of the applied field",
+			""
+	end
+		
+	if x == add then
+		return
+			"Add the direction and strength of the Applied Field",
+			"1 *3Vector*: The *3Vector* defines the strength and direction of the applied field addition",
+			""
+	end
+
+	if x == get then
+		return 
+		"Get the direction and strength of the Applied Field",
+		"",
+		"3 numbers: The x, y and z components of the field"
+	end
+		
+	if x == nil then
+		return help()
+	end
+	return help(x)
+end
+
