5ea32fe6cb6d5433a0c2d86f3d731ffa43c8b70a
Jason Mercer <jason.mercer@gmail.com>
2013-02-09 21:53:49 +0000

    forgot these
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@295 01fa2656-f03c-45d1-8fba-b86aa1ed0177

---
+++ src/modules/cpu/array/memory.h
@@ -0,0 +1,16 @@
+#ifndef ARRAY_MEMORY_H
+#define ARRAY_MEMORY_H
+
+
+#include "array_ops.h"
+
+ARRAY_API void  registerWS();
+ARRAY_API void  unregisterWS();
+
+// the level argument below prevents WSs from overlapping. This is useful for multi-level 
+// operations that all use WSs: example long range interaction. FFTs at lowest level with
+// a WS acting as an accumulator
+ARRAY_API bool getWSMem_(void** ptr, size_t size, long level);
+#define getWSMem(p1,s1,level) getWSMem_((void**)p1, s1, level)
+
+#endif
