7a4e7763967c9e5a162b34d8f303615ea7074e4b
Jason Mercer <jason.mercer@gmail.com>
2013-03-23 17:32:01 +0000

    looks like this wasn't in the repo.
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@310 01fa2656-f03c-45d1-8fba-b86aa1ed0177

---
+++ src/modules/cuda/magnetostatics2d_cuda/demag.cpp
@@ -0,0 +1,377 @@
+#include <math.h>
+#include <stdio.h>
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
+
+#ifndef M_PI
+#define M_PI 3.1415926535897932385
+#endif
+
+// JOURNAL OF GEOPHYSICAL RESEARCH, VOL. 98, NO. B6, PP. 9551-9555, 1993
+
+#if 1
+#define ATAN atanl
+#define SQRT sqrtl
+#define LOG  logl
+#define DOUBLE long double
+#else
+#define ATAN atan
+#define SQRT sqrt
+#define LOG  log
+#define DOUBLE double
+#endif
+
+
+
+
+static DOUBLE invtan(const DOUBLE num, const DOUBLE denom)
+{
+	if(denom == 0)
+	{
+		if(num > 0)
+			return M_PI * 0.5;
+		return -M_PI * 0.5;
+	}
+	const DOUBLE v = ATAN(num/denom);
+// 	if(isnan(v))
+// // 		printf("%Lf  %Lf\n", num, denom);
+	return v;
+}
+
+static DOUBLE sign(const DOUBLE x)
+{
+	if(x > 0)
+		return  1;
+	if(x < 0)
+		return -1;
+	return 0;
+}
+
+static DOUBLE phi(const DOUBLE n, const DOUBLE d)
+{
+	DOUBLE v =  0;
+	if(n == 0)
+		v = 0;
+	else
+	{
+		if(d == 0)
+			v = 1000 * sign(n); //arcsinh(10^500) ~= 1000. 10^500 is like infinity
+		else
+		{
+			const DOUBLE p = n / d;
+			const DOUBLE t = p + SQRT(1.0 + p*p);
+			if(t == 0)
+			{
+				v = 1000 * sign(n);
+			}
+			else
+				v = LOG(t);
+		}
+	}
+	return v;
+}
+
+
+static DOUBLE g(const DOUBLE x, const DOUBLE y, const DOUBLE z)
+{
+	const DOUBLE xx = x*x;
+	const DOUBLE yy = y*y;
+	const DOUBLE zz = z*z;
+	const DOUBLE R = SQRT(xx+yy+zz);
+
+	return 	  (x*y*z) * phi(z, SQRT(xx+yy))
+			+ (y/6.0) * (3.0*zz-yy) * phi(x, SQRT(yy+zz))
+			+ (x/6.0) * (3.0*zz-xx) * phi(y, SQRT(xx+zz))
+			- ( z*zz/6.0) * invtan(x*y,(z*R))
+			- ( z*yy/2.0) * invtan(x*z,(y*R))
+			- ( z*xx/2.0) * invtan(y*z,(x*R))
+			- (x*y*R/3.0);
+}
+
+static DOUBLE G2(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z) 
+{
+	return g(X,Y,Z) - g(X,Y,0);
+}
+static DOUBLE G1(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z,
+		 const DOUBLE dx2, const DOUBLE dy2, const DOUBLE dz2) 
+{
+	return G2(X+dx2,Y,Z+dz2) 
+		 - G2(X+dx2,Y,Z    )
+		 - G2(X,    Y,Z+dz2) 
+		 + G2(X,    Y,Z    );
+}
+
+static DOUBLE G(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z,
+		 const DOUBLE dx1, const DOUBLE dy1, const DOUBLE dz1, 
+		 const DOUBLE dx2, const DOUBLE dy2, const DOUBLE dz2) 
+{
+	return G1(X,Y,    Z,    dx2,dy2,dz2) 
+		 - G1(X,Y-dy1,Z,    dx2,dy2,dz2) 
+		 - G1(X,Y,    Z-dz1,dx2,dy2,dz2) 
+		 + G1(X,Y-dy1,Z-dz1,dx2,dy2,dz2);
+}
+
+
+
+
+
+static DOUBLE f(const DOUBLE x, const DOUBLE y, const DOUBLE z)
+{
+	const DOUBLE xx = x*x;
+	const DOUBLE yy = y*y;
+	const DOUBLE zz = z*z;
+	const DOUBLE R = SQRT(xx+yy+zz);
+	
+	return 	  y * 0.5 * (zz-xx) * phi(y,SQRT(xx+zz))
+			+ z * 0.5 * (yy-xx) * phi(z,SQRT(xx+yy))
+			- x*y*z * invtan(y*z, x*R)
+			+ (1.0/6.0) * (2.0*xx-yy-zz) * R;
+}
+
+static DOUBLE F2(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z,
+		 const DOUBLE dx1, const DOUBLE dy1, const DOUBLE dz1, 
+		 const DOUBLE dx2, const DOUBLE dy2, const DOUBLE dz2) 
+{
+	const DOUBLE v = 
+	       f(X,Y,Z) - f(0,Y,Z) - f(X,0,Z) + f(X,Y,0);
+	return v;
+}
+
+static DOUBLE F1(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z,
+		 const DOUBLE dx1, const DOUBLE dy1, const DOUBLE dz1, 
+		 const DOUBLE dx2, const DOUBLE dy2, const DOUBLE dz2) 
+{
+	const DOUBLE v =
+	        F2(X, Y,     Z,     dx1, dy1, dz1, dx2, dy2, dz2)
+	      - F2(X, Y-dy1, Z,     dx1, dy1, dz1, dx2, dy2, dz2)
+		  - F2(X, Y,     Z-dz1, dx1, dy1, dz1, dx2, dy2, dz2)
+		  + F2(X, Y-dy1, Z-dz1, dx1, dy1, dz1, dx2, dy2, dz2);
+	return v;
+}
+
+static DOUBLE F(const DOUBLE X,   const DOUBLE Y,   const DOUBLE Z,
+		 const DOUBLE dx1, const DOUBLE dy1, const DOUBLE dz1, 
+		 const DOUBLE dx2, const DOUBLE dy2, const DOUBLE dz2) 
+{
+	const DOUBLE v = 
+	         F1(X, Y+dy2, Z+dz2, dx1, dy1, dz1, dx2, dy2, dz2) 
+		   - F1(X, Y,     Z+dz2, dx1, dy1, dz1, dx2, dy2, dz2)  
+		   - F1(X, Y+dy2, Z,     dx1, dy1, dz1, dx2, dy2, dz2)  
+		   + F1(X, Y,     Z,     dx1, dy1, dz1, dx2, dy2, dz2);	
+		   
+	return v;
+}
+
+static void shuffle(double* dest, const double* src, const int* o)
+{
+	dest[0] = src[o[0]];
+	dest[1] = src[o[1]];
+	dest[2] = src[o[2]];
+}
+
+
+double magnetostatic_Nxx(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	const double* p1 = target;
+	const double* p2 = source;
+	const DOUBLE dx1 = p1[0];
+	const DOUBLE dx2 = p2[0];
+	const DOUBLE v = p1[0]*p1[1]*p1[2];
+	
+	const DOUBLE FF1 = F(X,         Y, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+	const DOUBLE FF2 = F(X+dx2-dx1, Y, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+	const DOUBLE FF3 = F(X-dx1,     Y, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+	const DOUBLE FF4 = F(X+dx2,     Y, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]);
+
+	return -(1.0/(4.0*M_PI * v)) * (FF1 + FF2 - FF3 - FF4);
+}
+
+
+double magnetostatic_Nyy(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {1,2,0};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+
+	return magnetostatic_Nxx(Y, Z, X, t, s);
+}
+
+double magnetostatic_Nzz(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {2,0,1};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+
+	return magnetostatic_Nxx(Z, X, Y, t, s);
+}
+
+
+double magnetostatic_Nxy(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	const double* p1 = target;
+	const double* p2 = source;
+	const DOUBLE dx1 = p1[0];
+	const DOUBLE dx2 = p2[0];
+	const DOUBLE dy1 = p1[1];
+	const DOUBLE dy2 = p2[1];
+	const DOUBLE v = p1[0]*p1[1]*p1[2];
+	return -(1.0 / (4.0*M_PI*v)) * (
+		  G(X,     Y,     Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]) 
+		- G(X-dx1, Y,     Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]) 
+		- G(X,     Y+dy2, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]) 
+		+ G(X-dx1, Y+dy2, Z, p1[0], p1[1], p1[2], p2[0], p2[1], p2[2]));
+}
+
+
+double magnetostatic_Nxz(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {0,2,1};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+	return magnetostatic_Nxy(X, Z, Y, t, s);
+}
+
+double magnetostatic_Nzx(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {2,0,1};
+	
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+	return magnetostatic_Nxy(Z, X, Y, t, s);
+}
+
+double magnetostatic_Nyx(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {1,0,2};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+	return magnetostatic_Nxy(Y, X, Z, t, s);
+}
+
+double magnetostatic_Nyz(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {1,2,0};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+	return magnetostatic_Nxy(Y, Z, X, t, s);
+}
+
+
+double magnetostatic_Nzy(const double X,   const double Y,   const double Z,
+				  const double* target, const double* source)
+{
+	double t[3], s[3];
+	const int o[3] = {2,1,0};
+	shuffle(t, target, o);
+	shuffle(s, source, o);
+	return magnetostatic_Nxy(Z, Y, X, t, s);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define PROTO(func) static int l_ ## func(lua_State* L) {
+
+#define FUNC1(func) \
+PROTO(func) \
+	const DOUBLE v1 = lua_tonumber(L, 1); \
+	lua_pushnumber(L,  func(v1) ); return 1; }
+
+#define FUNC2(func) \
+PROTO(func) \
+	const DOUBLE v1 = lua_tonumber(L, 1); \
+	const DOUBLE v2 = lua_tonumber(L, 2); \
+	lua_pushnumber(L,  func(v1,v2) ); return 1; }
+
+#define FUNC3(func) \
+PROTO(func) \
+	const DOUBLE v1 = lua_tonumber(L, 1); \
+	const DOUBLE v2 = lua_tonumber(L, 2); \
+	const DOUBLE v3 = lua_tonumber(L, 3); \
+	lua_pushnumber(L,  func(v1,v2,v3) ); return 1; }
+
+#define FUNC6(func) \
+PROTO(func) \
+	const DOUBLE v1 = lua_tonumber(L, 1); \
+	const DOUBLE v2 = lua_tonumber(L, 2); \
+	const DOUBLE v3 = lua_tonumber(L, 3); \
+	const DOUBLE v4 = lua_tonumber(L, 4); \
+	const DOUBLE v5 = lua_tonumber(L, 5); \
+	const DOUBLE v6 = lua_tonumber(L, 6); \
+	lua_pushnumber(L,  func(v1,v2,v3,v4,v5,v6) ); return 1; }
+
+#define FUNC9(func) \
+PROTO(func) \
+	const DOUBLE v1 = lua_tonumber(L, 1); \
+	const DOUBLE v2 = lua_tonumber(L, 2); \
+	const DOUBLE v3 = lua_tonumber(L, 3); \
+	const DOUBLE v4 = lua_tonumber(L, 4); \
+	const DOUBLE v5 = lua_tonumber(L, 5); \
+	const DOUBLE v6 = lua_tonumber(L, 6); \
+	const DOUBLE v7 = lua_tonumber(L, 7); \
+	const DOUBLE v8 = lua_tonumber(L, 8); \
+	const DOUBLE v9 = lua_tonumber(L, 9); \
+	lua_pushnumber(L,  func(v1,v2,v3,v4,v5,v6,v7,v8,v9) ); return 1; }
+
+
+
+FUNC1(sign)
+FUNC2(invtan)
+FUNC2(phi)
+FUNC3(g)
+FUNC3(f)
+FUNC3(G2)
+FUNC6(G1)
+FUNC9(G)
+FUNC9(F)
+FUNC9(F1)
+FUNC9(F2)
+
+#define REG(func) lua_pushstring(L, #func); lua_pushcfunction(L, l_##func); lua_settable(L, -3);
+
+void register_mag2d_internal_functions(lua_State* L)
+{
+	lua_getglobal(L, "Magnetostatics2D");
+	
+	REG(sign);
+	REG(invtan);
+	REG(phi);
+	REG(g);
+	REG(f);
+	REG(G1);
+	REG(G2);
+	REG(G);
+	REG(F1);
+	REG(F2);
+	REG(F);
+	
+	lua_pop(L, 1);
+}
+
---
+++ src/modules/cuda/magnetostatics2d_cuda/spinoperationmagnetostatics2d.cpp
@@ -0,0 +1,257 @@
+/******************************************************************************
+* Copyright (C) 2008-2011 Jason Mercer.  All rights reserved.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+******************************************************************************/
+
+#include "spinsystem.h"
+#include "info.h"
+#include "spinoperationmagnetostatics2d.h"
+
+
+Magnetostatics2D::Magnetostatics2D(const char* Name, const int field_slot, int nx, int ny, int nz, const int encode_tag)
+	: LongRange2D(Name, field_slot, nx, ny, nz, encode_tag)
+{
+}
+
+Magnetostatics2D::~Magnetostatics2D()
+{
+}
+
+// this is a base 0 function
+double Magnetostatics2D::getGrainSize(const int layer)
+{
+	// info in longrange_ref
+	lua_rawgeti(L, LUA_REGISTRYINDEX, longrange_ref);
+	
+	lua_pushstring(L, "grainSize"); 
+	lua_gettable(L, -2);// grain size table on stack
+	
+	if(layer >= nz || layer < 0)
+		luaL_error(L, "getGrainSize: invalid layer");
+	
+	lua_pushinteger(L, layer+1);
+	lua_gettable(L, -2); // grain size for requested table on stack
+		
+	double xyz[3];
+	
+	for(int i=0; i<3; i++)
+	{
+		lua_pushinteger(L, i+1);
+		lua_gettable(L, -2);
+		xyz[i] = lua_tonumber(L, -1);
+		lua_pop(L, 1);
+	}
+	lua_pop(L, 3);
+	
+	return xyz[0] * xyz[1] * xyz[2];
+	
+}
+
+
+void Magnetostatics2D::push(lua_State* L)
+{
+	luaT_push<Magnetostatics2D>(L, this);
+}
+
+int Magnetostatics2D::luaInit(lua_State* L)
+{
+	LongRange2D::luaInit(L);
+	lua_getglobal(L, "Magnetostatics2D");
+	lua_getfield(L, -1, "internalSetup");
+	luaT_push<Magnetostatics2D>(L, this);
+	lua_call(L, 1, 0);
+	return 0;
+}
+
+int Magnetostatics2D::help(lua_State* L)
+{
+	if(lua_gettop(L) == 0)
+	{
+		lua_pushstring(L, "Calculates a Long Range Magnetostatic field for a *SpinSystem*.");
+		lua_pushstring(L, "1 *3Vector* or *SpinSystem*: System Size"); 
+		lua_pushstring(L, ""); //output, empty
+		return 3;
+	}
+	
+	return LongRange2D::help(L);
+}
+
+static luaL_Reg m[128] = {_NULLPAIR128};
+const luaL_Reg* Magnetostatics2D::luaMethods()
+{
+	if(m[127].name)return m;
+
+	merge_luaL_Reg(m, LongRange2D::luaMethods());
+	static const luaL_Reg _m[] =
+	{
+		{NULL, NULL}
+	};
+	merge_luaL_Reg(m, _m);
+	m[127].name = (char*)1;
+	return m;
+}
+
+
+
+
+
+extern "C"
+{
+MAGNETOSTATICS2D_API int lib_register(lua_State* L);
+MAGNETOSTATICS2D_API int lib_version(lua_State* L);
+MAGNETOSTATICS2D_API const char* lib_name(lua_State* L);
+MAGNETOSTATICS2D_API int lib_main(lua_State* L);
+}
+
+#include "mag2d_luafuncs.h"
+
+static int l_getmetatable(lua_State* L)
+{
+	if(!lua_isstring(L, 1))
+		return luaL_error(L, "First argument must be a metatable name");
+	luaL_getmetatable(L, lua_tostring(L, 1));
+	return 1;
+}
+
+#include "demag.h"
+#define ___(F)					   \
+	double X = lua_tonumber(L, 1); \
+	double Y = lua_tonumber(L, 2); \
+	double Z = lua_tonumber(L, 3); \
+ \
+	double p[2][3]; \
+	int q[2] = {4,5}; \
+	for(int i=0; i<2; i++) \
+	{ \
+		for(int j=0; j<3; j++) \
+		{ \
+			lua_pushinteger(L, 1+j); \
+			lua_gettable(L, q[i]); \
+			p[i][j] = lua_tonumber(L, -1); \
+			lua_pop(L, 1); \
+		} \
+	} \
+	lua_pushnumber(L, F(X, Y, Z, p[0], p[1])); \
+	return 1; \
+	
+
+static int l_xx(lua_State* L){ ___(magnetostatic_Nxx) }
+static int l_xy(lua_State* L){ ___(magnetostatic_Nxy) }
+static int l_xz(lua_State* L){ ___(magnetostatic_Nxz) }
+
+static int l_yx(lua_State* L){ ___(magnetostatic_Nyx) }
+static int l_yy(lua_State* L){ ___(magnetostatic_Nyy) }
+static int l_yz(lua_State* L){ ___(magnetostatic_Nyz) }
+
+static int l_zx(lua_State* L){ ___(magnetostatic_Nzx) }
+static int l_zy(lua_State* L){ ___(magnetostatic_Nzy) }
+static int l_zz(lua_State* L){ ___(magnetostatic_Nzz) }
+
+
+
+#include "pointfunc_demag.h"
+#define __p(F)					   \
+	double X = lua_tonumber(L, 1); \
+	double Y = lua_tonumber(L, 2); \
+	double Z = lua_tonumber(L, 3); \
+ \
+	double s[3]; \
+	for(int j=0; j<3; j++) \
+	{ \
+		lua_pushinteger(L, 1+j); \
+		lua_gettable(L, 4); \
+		s[j] = lua_tonumber(L, -1); \
+		lua_pop(L, 1); \
+	} \
+	lua_pushnumber(L, F(X, Y, Z, s)); \
+	return 1; \
+	
+static int l_pxx(lua_State* L){ __p(magnetostatic_Pxx) }
+static int l_pxy(lua_State* L){ __p(magnetostatic_Pxy) }
+static int l_pxz(lua_State* L){ __p(magnetostatic_Pxz) }
+
+static int l_pyx(lua_State* L){ __p(magnetostatic_Pyx) }
+static int l_pyy(lua_State* L){ __p(magnetostatic_Pyy) }
+static int l_pyz(lua_State* L){ __p(magnetostatic_Pyz) }
+
+static int l_pzx(lua_State* L){ __p(magnetostatic_Pzx) }
+static int l_pzy(lua_State* L){ __p(magnetostatic_Pzy) }
+static int l_pzz(lua_State* L){ __p(magnetostatic_Pzz) }
+
+MAGNETOSTATICS2D_API int lib_register(lua_State* L)
+{
+	luaT_register<Magnetostatics2D>(L);
+
+	lua_getglobal(L, "Magnetostatics2D");
+	
+	lua_pushcfunction(L, l_xx);	lua_setfield(L, -2, "NXX");
+	lua_pushcfunction(L, l_xy);	lua_setfield(L, -2, "NXY");
+	lua_pushcfunction(L, l_xz);	lua_setfield(L, -2, "NXZ");
+
+	lua_pushcfunction(L, l_yx);	lua_setfield(L, -2, "NYX");
+	lua_pushcfunction(L, l_yy);	lua_setfield(L, -2, "NYY");
+	lua_pushcfunction(L, l_yz);	lua_setfield(L, -2, "NYZ");
+
+	lua_pushcfunction(L, l_zx);	lua_setfield(L, -2, "NZX");
+	lua_pushcfunction(L, l_zy);	lua_setfield(L, -2, "NZY");
+	lua_pushcfunction(L, l_zz);	lua_setfield(L, -2, "NZZ");
+	
+		
+	lua_pushcfunction(L, l_pxx);	lua_setfield(L, -2, "PXX");
+	lua_pushcfunction(L, l_pxy);	lua_setfield(L, -2, "PXY");
+	lua_pushcfunction(L, l_pxz);	lua_setfield(L, -2, "PXZ");
+
+	lua_pushcfunction(L, l_pyx);	lua_setfield(L, -2, "PYX");
+	lua_pushcfunction(L, l_pyy);	lua_setfield(L, -2, "PYY");
+	lua_pushcfunction(L, l_pyz);	lua_setfield(L, -2, "PYZ");
+
+	lua_pushcfunction(L, l_pzx);	lua_setfield(L, -2, "PZX");
+	lua_pushcfunction(L, l_pzy);	lua_setfield(L, -2, "PZY");
+	lua_pushcfunction(L, l_pzz);	lua_setfield(L, -2, "PZZ");
+	lua_pop(L, 1); //pop table
+	
+	register_mag2d_internal_functions(L);
+	
+	lua_pushcfunction(L, l_getmetatable);
+	lua_setglobal(L, "maglua_getmetatable");
+	if(luaL_dostring(L, __mag2d_luafuncs()))
+	{
+		fprintf(stderr, "%s\n", lua_tostring(L, -1));
+		return luaL_error(L, lua_tostring(L, -1));
+	}
+
+	lua_pushnil(L);
+	lua_setglobal(L, "maglua_getmetatable");
+
+	return 0;
+}
+
+
+MAGNETOSTATICS2D_API int lib_version(lua_State* L)
+{
+	return __revi;
+}
+
+MAGNETOSTATICS2D_API const char* lib_name(lua_State* L)
+{
+#if defined NDEBUG || defined __OPTIMIZE__
+	return "Magnetostatics2D-Cuda";
+#else
+	return "Magnetostatics2D-Cuda-Debug";
+#endif
+}
+
+MAGNETOSTATICS2D_API int lib_main(lua_State* L)
+{
+	return 0;
+}
+
+
+
