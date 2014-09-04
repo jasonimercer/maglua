15efdbb7f193c6ddd716a29ca8d77e983e7ae38b
Jason Mercer <jason.mercer@gmail.com>
2014-09-04 12:37:12 +0000

    changed mpi c++ compiler for orcinus.
    git-svn-id: svn+ssh://cmms01.ace-net.ca/users/cmms/jmercer/programming/repos/maglua@372 01fa2656-f03c-45d1-8fba-b86aa1ed0177

--- src/modules/extras/sqlite3/example_setup.lua
+++ src/modules/extras/sqlite3/example_setup.lua
@@ -10,29 +10,28 @@ sql:exec("INSERT INTO People VALUES(NULL, 'Ellen', 4, 'blue',  0);")
 sql:exec("INSERT INTO People VALUES(NULL, 'Frank', 6, 'indigo',0);")
 sql:exec("INSERT INTO People VALUES(NULL, 'Gloria',7, 'violet',0);")
 
-
 -- bootstrap function, must be named "init" expected to take an active database
 function init(s)
-	local function checkout(sql) --custom checkout function for this data/problem
-		local res = sql:exec("SELECT * FROM People WHERE registered=0 LIMIT 1;")
-
-		if res[1] then
-			sql:exec("UPDATE People SET registered=-1 WHERE registered=0 AND id="..res[1].id)
-			if sql:changes() > 0 then
-				return res[1].id
-			end
-		end
-	end
-	
-	local function checkin(sql, id) --custom checkin function for this data/problem
-		if id then
-			sql:exec("UPDATE People SET registered=1 WHERE registered=-1 AND id="..id) --reg = -1 : pending
-			return sql:changes() > 0
-		end
-		return false
-	end
-	
-	return checkout, checkin
+    local function checkout(sql) --custom checkout function for this data/problem
+        local res = sql:exec("SELECT * FROM People WHERE registered=0 LIMIT 1;")
+        
+        if res[1] then
+            sql:exec("UPDATE People SET registered=-1 WHERE registered=0 AND id="..res[1].id)
+            if sql:changes() > 0 then
+                return res[1].id
+            end
+        end
+    end
+    
+    local function checkin(sql, id) --custom checkin function for this data/problem
+        if id then
+            sql:exec("UPDATE People SET registered=1 WHERE registered=-1 AND id="..id) --reg = -1 : pending
+            return sql:changes() > 0
+        end
+        return false
+    end
+    
+    return checkout, checkin
 end
 
 sql:setupBootstrap(init)
--- src/modules/extras/sqlite3/example_test.lua
+++ src/modules/extras/sqlite3/example_test.lua
@@ -4,8 +4,8 @@ checkout, checkin = sql:bootstrap()
 id = checkout(sql)
 
 if id then
-	res = sql:exec("SELECT * FROM People WHERE id="..id..";")
-	print(res[1].id, res[1].name)
+    res = sql:exec("SELECT * FROM People WHERE id="..id..";")
+    print(res[1].id, res[1].name)
 end
 
 checkin(sql, id)
